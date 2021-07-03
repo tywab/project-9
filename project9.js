e5c785eb7e5e1792737f57079e5436c272add72f
tywab <86836839+tywab@users.noreply.github.com>
2021-07-03 18:10:21 +0530

    Add files via upload

---
+++ project9.js
@@ -0,0 +1,21 @@
+var box
+function setup() {
+  createCanvas(400,400);
+box=createSprite(200,200,400,400)
+box.shapeColor="red"
+}
+
+function draw() 
+{
+  background("red");
+  if(keyDown(DOWN_ARROW))
+  {
+    box.shapeColor="yellow";
+
+  }
+drawSprites();
+}
+
+
+
+
